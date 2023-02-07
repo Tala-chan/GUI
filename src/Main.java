604509fe278bf3a08a0aa7058b062eed263d4d8b
Tala-chan <92362403+Tala-chan@users.noreply.github.com>
2023-02-07 23:35:56 +0100

    Change font

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
-
+import java.io.File;
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         ImageIcon image = new ImageIcon("Kanika idea.png");
+        Font RetGanon = null;
+
+        try{
+            RetGanon = Font.createFont(Font.TRUETYPE_FONT, new File("RetGanon.ttf")).deriveFont(30f);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("RetGanon.ttf")));
+        }
+        catch(IOException | FontFormatException e){}
 
         JLabel label = new JLabel();
         label.setText("Do you even lift bro?");
@@ -12,7 +21,7 @@ public class Main {
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.TOP);
         label.setForeground(Color.blue);
-        label.setFont(new Font("RetGanon.ttf", Font.PLAIN,20));
+        label.setFont(RetGanon);
 
         MyFrame myFrame = new MyFrame();
         myFrame.add(label);
