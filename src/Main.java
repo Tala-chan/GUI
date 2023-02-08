45febb9d823a90ea392424c105268f4ce3e0c3ca
Tala-chan <92362403+Tala-chan@users.noreply.github.com>
2023-02-09 00:42:19 +0100

    Label class

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,14 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        ImageIcon image = new ImageIcon("Kanika idea.png");
-        Font RetGanon = null;
-
-        try{
-            RetGanon = Font.createFont(Font.TRUETYPE_FONT, new File("RetGanon.ttf")).deriveFont(30f);
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("RetGanon.ttf")));
-        }
-        catch(IOException | FontFormatException e){}
-
-        JLabel label = new JLabel();
-        label.setText("Do you even lift bro?");
-        label.setIcon(image);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        label.setForeground(Color.blue);
-        label.setFont(RetGanon);
-
+        /*
         MyFrame myFrame = new MyFrame();
-        myFrame.add(label);
+         */
+        MyLabel myLabel = new MyLabel();
     }
 }
